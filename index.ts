5d98af849c0fc141f35c156b9d315f0f0f6c52d9
alexsandersarmento <alexsander.br18@gmail.com>
2023-05-24 14:55:28 -0300

    refactor: improving the methods to avoid possible bugs

--- index.ts
+++ index.ts
@@ -1,31 +1,20 @@
-const listenersMap: { [id: string]: Array<(...params: any[]) => void> } = {};
+let listenersMap: { [id: string]: ((...params: any[]) => void) } = {};
 
 export const addListener = (eventName: string, listener: (...params: any[]) => void) => {
-  listenersMap[eventName] = listenersMap[eventName] || [];
-  listenersMap[eventName].push(listener);
+  listenersMap[eventName] = listener;
 };
 
-export const removeListener = (eventName: string, listener: (...params: any[]) => void) => {
-  const lis = listenersMap[eventName];
-  if (!lis) return;
-
-  for (let i = lis.length - 1; i >= 0; i--) {
-    if (lis[i] === listener) {
-      lis.splice(i, 1);
-      break;
-    }
-  }
+export const removeListener = (eventName: string) => {
+  delete listenersMap[eventName];
 };
 
-export const removeAllListeners = (eventName: string) => {
-  listenersMap[eventName] = [];
+export const removeAllListeners = () => {
+  listenersMap = {};
 };
 
 export const notify = <T = any>(eventName: string, ...params: T[]) => {
-  const listeners = listenersMap[eventName];
-  if (!listeners) return false;
-  listeners.forEach(fnc => {
-    fnc(...params);
-  });
+  const listener = listenersMap[eventName];
+  if (!listener) return false;
+  listener(...params);
   return true;
 };
